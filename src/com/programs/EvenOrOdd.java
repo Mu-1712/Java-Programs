6a20d6cd8524ee21aa8567428c71453828372139
Mu-1712 <musharafmohammed460@gmail.com>
2024-04-26 13:31:09 +0530

    commit

--- src/com/programs/EvenOrOdd.java
+++ src/com/programs/EvenOrOdd.java
@@ -2,20 +2,26 @@ package com.programs;
 
 import java.util.Scanner;
 
-class EvenOdd{
+class EvenOdd {
     public static void main(String[] args) {
-        int n;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter a number: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number or character: ");
 
-            n = sc.nextInt();
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
             if (n % 2 == 0)
-                System.out.println("Entered number is a Even Number.");
+                System.out.println("Entered number is Even.");
             else
-
-                System.out.println("Entered number is a Odd Number.");
-
-
+                System.out.println("Entered number is Odd.");
+        } else if (sc.hasNextFloat()) {
+            float f = sc.nextFloat();
+            int n = (int) f; // Convert float to int
+            if (n % 2 == 0)
+                System.out.println("Entered float value is Even.");
+            else
+                System.out.println("Entered float value is Odd.");
+        } else {
+            System.out.println("Invalid input.");
         }
     }
-
+}
