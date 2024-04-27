277ebbfd023a8a75f428e5c2bcd95ac19ffaa2fa
Mu-1712 <musharafmohammed460@gmail.com>
2024-04-27 13:59:38 +0530

    commit

--- src/com/programs/Palindrome_Armstrong.java
+++ src/com/programs/Palindrome_Armstrong.java
@@ -1,28 +1,49 @@
 package com.programs;
-
 import java.util.Scanner;
 
 public class Palindrome_Armstrong {
     public static void main(String [] args){
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
+        
         int num = sc.nextInt();
         if (isPalindrome(num)) {
-            System.out.println("The input number is a palindrome.");
+            System.out.println(num + " is a palindrome.");
         } else {
-            System.out.println("The input number is not a palindrome.");
+            System.out.println(num + " is not a palindrome.");
+        }
+        if(isArmstrong(num)){
+            System.out.println(num + " is Armstrong number");
+        }else{
+            System.out.println(num + " not a Armstrong number");
         }
     }
 
     private static boolean isPalindrome(int num) {
         int temp = num;
-        int revnum = 0;
+        int rem = 0;
         while (temp != 0) {
             int digit = temp % 10;
-            revnum = revnum * 10 + digit;
+            rem = rem * 10 + digit;
             temp = temp / 10;
         }
-        return num == revnum;
+        return num == rem;
     }
 
+    private static boolean isArmstrong(int num) {
+        int number = num, rem, result = 0, n=0;
+        while(num!=0){
+            num/=10;
+            ++n;
+        }
+        num = number;
+        while (num!=0){
+            rem = num % 10;
+            result += Math.pow(rem, n);
+            num /= 10;
+        }
+        if (result == number){return true;}
+        else {return false;}
+    }
 }
